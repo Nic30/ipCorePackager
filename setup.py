2970271c9f4c4115fe9cc9ed3a0210aceb8ce5de
Nic30 <nic30@seznam.cz>
2020-02-25 18:55:50 +0100

    v0.4

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(name='ipCorePackager',
-      version='0.3',
+      version='0.4',
       description='Scriptable universal IP-core generator',
       long_description=long_description,
       long_description_content_type="text/markdown",
